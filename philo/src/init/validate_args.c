b09e8b004fe29b0c8b6afeb562460fefe9627ded
Yuya Oda <yoda@c1r14s4.42tokyo.jp>
2024-03-17 21:11:36 +0900

    a

--- philo/src/init/validate_args.c
+++ philo/src/init/validate_args.c
@@ -6,7 +6,7 @@
 /*   By: yoda <yoda@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 03:08:54 by yoda              #+#    #+#             */
-/*   Updated: 2024/02/19 16:32:58 by yoda             ###   ########.fr       */
+/*   Updated: 2024/03/17 21:11:15 by yoda             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ bool	validate_args(int argc, char **argv)
 	}
 	if (ft_atoi(argv[1]) < 1 || 200 < ft_atoi(argv[1]))
 		return (false);
-	if (ft_atoi(argv[2]) < 10 || ft_atoi(argv[3]) < 10 || ft_atoi(argv[4]) < 10)
+	if (ft_atoi(argv[2]) < 60 || ft_atoi(argv[3]) < 60 || ft_atoi(argv[4]) < 60)
 		return (false);
 	if (argc == 6 && ft_atoi(argv[5]) < 1)
 		return (false);
